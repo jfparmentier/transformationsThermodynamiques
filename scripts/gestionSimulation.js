e06a608077973684f6704385b09d4a5a0ef00fff
jfparmentier <jfparmentier@gmail.com>
2023-05-21 21:10:00 +0200

    gestion blocage brutale

--- scripts/gestionSimulation.js
+++ scripts/gestionSimulation.js
@@ -104,7 +104,9 @@ function start_transformation() {
     var pression_finale;
     var temperature_finale;
     var hauteur_finale;
-
+    let hauteur_finale_libre;
+    let fmax = 1; // par defaut le piston s'arrete à accélération nulle
+    
     if(piston_libre) {
         // equilibre mecanique
         pression_finale = poids_masses.reduce(function (accumulateur, valeurCourante, index) {
@@ -122,9 +124,29 @@ function start_transformation() {
         
         // loi des gaz parfaits
         hauteur_finale = hauteur_piston * pression  / pression_finale  * temperature_finale / temperature;
+        hauteur_finale_libre = hauteur_finale;
+
+        // blocage hauteur finale
+        if ((hauteur_finale > 1) || (hauteur_finale < 0.1)) {
+            hauteur_finale = Math.max(Math.min(hauteur_finale, 1), 0.1);
+            // calcul du fmax pour l'animation (car blocage)
+            fmax = (hauteur_finale - hauteur_piston)/(hauteur_finale_libre - hauteur_piston);            
+
+            if(adiabatique) {
+                // on suppose que le travail c'est -(Patm + Masses) (Vf - Vi)
+                let pression_masses = pression_finale;
+                let r_hauteurs = hauteur_piston / hauteur_finale;
+                pression_finale = pression * r_hauteurs - pression_masses/cv * (1 - r_hauteurs);
+                // loi des gaz parfaits
+                temperature_finale = temperature * (pression_finale * hauteur_finale)/(pression*hauteur_piston);
+            } else {
+                pression_finale = pression * temperature_finale / temperature * hauteur_piston / hauteur_finale;
+            }
+        }
     } else {
         // transformation isochore
         hauteur_finale = hauteur_piston;
+        hauteur_finale_libre = hauteur_piston;
 
         if(adiabatique) {
             // le piston ne de deplace pas et il n'y a pas d'echanges de chaleur
@@ -146,14 +168,16 @@ function start_transformation() {
         duree_simulation = 1000;
     }
 
+
     let start = null;
     function step(timestamp) {
         if (start === null) start = timestamp;
         let progress = (timestamp - start) / duree_simulation;
 
         let x = Math.min(progress, 1);
-        let h_t =  hauteur_piston + x * (2 - x) * (1 + (1-x)**2) * (hauteur_finale - hauteur_piston);
-        
+        let f = Math.min(x * (2 - x) * (1 + (1-x)**2), fmax);
+        let h_t =  hauteur_piston + f * (hauteur_finale_libre - hauteur_piston);
+
         // deplace le piston au bon endroit
         var element_piston = document.getElementById("piston_cale");
         element_piston.setAttribute("transform", 'translate(0 ' + dy_piston_from_hauteur(h_t) + ')')
@@ -167,7 +191,7 @@ function start_transformation() {
             }
         }
 
-        if(progress <= 1) {
+        if((progress <= 1) && f < fmax) {
             requestAnimationFrame(step);
         } else {
             pression = pression_finale;
